8f3d171f82d59a3aa96d7025602ac09bd53ad135
Yan <yan.i.andrade@hotmail.com>
2020-06-22 23:06:22 -0300

    Pasta Escolher ok / Pasta Futebol +/-

---
+++ Escolher/escolher.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+<link rel="stylesheet" href="escolher.css" type="text/css">
+<link rel="stylesheet" href="fontes/font-awesome.min.css">
+
+<script src="https://kit.fontawesome.com/54f9cce8ca.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css" integrity="sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1" crossorigin="anonymous">
+
+
+</head>
+<body>
+
+<!----------------------------------------Navbar------------------------------------------>
+
+    <header class="header">  
+        <nav class="navbar navbar-default fixed-top">
+            <div class="container">
+                <div class="navbar-header" >
+                    <a href="#" class="navbar-brand" id="logo"> <img src="Imagens/Anota (1).png" alt=""></a>
+                </div>
+                <ul class="nav navbar-icones">
+                    <div class="header-buttons text-center">
+                        <a href="../Cadastro/cadastro.php" class="btn btn-half">Sair</a>
+                    </div>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+<!----------------------------------------Quem Somos------------------------------------------>
+<br>
+    <section class="offer">
+        <div class="container-fluid">
+            <br>
+            <h2 class="text-center "> Bem vindo Jogador </h2>
+        </div>
+        <div>
+            <p class="description"> 
+               <h3 class="text-center ">Qual esporte iremos escolher?</h3>
+            </p>
+        </div>  
+    </section>
+<!---------------------------------------------Atletas---+------------------------------------------------->
+<section class="testimonials"> 
+            <div class="conatiner">
+                <div class="wrap">
+                    <div class="box one">
+                        <div class="date">
+                        </div>
+                        <h1>FUTEBOL</h1>
+                        <div class="text-box">
+                        <div class="container">
+                            <div class="center">
+                                <button class="btn1">
+                                    <svg width="180px" height="60px" viewBox="0 0 180 60" class="border">
+                                    <polyline points="179,1 179,59 1,59 1,1 179,1" class="bg-line" />
+                                    <polyline points="179,1 179,59 1,59 1,1 179,1" class="hl-line" />
+                                    </svg>
+                                    <a href="../Futebol/futebol.php">Entrar</a>
+                                </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                <div class="box two">
+                    <div class="date">
+                    </div>
+                    <H1>BASQUETE</H1>
+                    <div class="container">
+                            <div class="center">
+                                <button class="btn1">
+                                    <svg width="180px" height="60px" viewBox="0 0 180 60" class="border">
+                                    <polyline points="179,1 179,59 1,59 1,1 179,1" class="bg-line" />
+                                    <polyline points="179,1 179,59 1,59 1,1 179,1" class="hl-line" />
+                                    </svg>
+                                    <a href="#">Entrar</a>
+                                </button>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
+    </section>
+</section>
+<!--------------------------------------teste------------------------------------------------------------->
